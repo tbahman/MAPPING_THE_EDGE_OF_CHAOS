168cc04984238c73c3c575cb605500c454e291b2
tbahman <66690650+tbahman@users.noreply.github.com>
2024-12-09 14:26:37 -0600

    Create preprocessing.py
    clean_gutenberg_text

---
+++ utils/preprocessing.py
@@ -0,0 +1,6 @@
+def clean_gutenberg_text(text):
+    start_marker = "*** START OF THIS PROJECT GUTENBERG EBOOK"
+    end_marker = "*** END OF THIS PROJECT GUTENBERG EBOOK"
+    start_idx = text.find(start_marker)
+    end_idx = text.find(end_marker)
+    return text[start_idx + len(start_marker) : end_idx].strip() if start_idx != -1 and end_idx != -1 else text
