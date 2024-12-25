1309f24797a0c2722e63f39482075bb73be87b1d
tbahman <66690650+tbahman@users.noreply.github.com>
2024-12-24 21:01:10 -0700

    Update checkpoint.py:save_checkpoint

--- utils/checkpoint.py
+++ utils/checkpoint.py
@@ -2,3 +2,9 @@ import json
 import os
 from utils.serialization import convert_to_serializable
 
+def save_checkpoint(current_index, results, args_adls_output, checkpoint=1):
+    checkpoint_data = {"current_index": current_index, "results": convert_to_serializable(results)}
+    checkpoint_file = f"{args_adls_output}/checkpoint_{checkpoint}.json"
+    with open(checkpoint_file, "w") as f:
+        json.dump(checkpoint_data, f)
+
