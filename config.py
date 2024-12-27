953abe5e56b57b3f77c2469ba56940af39befeca
tbahman <66690650+tbahman@users.noreply.github.com>
2024-12-26 18:00:45 -0700

    Update config.py:argparse

--- config.py
+++ config.py
@@ -1,1 +1,13 @@
 
+
+
+import argparse
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument(
+    "--adls_output", default=None, help="Path of the folder on ADLS for output files"
+)
+ARGS = parser.parse_args()
+
+
+
