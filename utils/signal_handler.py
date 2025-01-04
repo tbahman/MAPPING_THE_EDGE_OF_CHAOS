67d27848503303663783b31b9966547d91009b1f
tbahman <66690650+tbahman@users.noreply.github.com>
2025-01-04 17:01:04 -0600

    Update signal_handler.py:handle_preemption

--- utils/signal_handler.py
+++ utils/signal_handler.py
@@ -1,3 +1,7 @@
 import signal
 from utils.checkpoint import save_checkpoint
 
+def handle_preemption(signum, frame, current_index, results, args_adls_output, checkpoint_ver):
+    save_checkpoint(current_index, results, args_adls_output, checkpoint_ver)
+    exit(0)
+
