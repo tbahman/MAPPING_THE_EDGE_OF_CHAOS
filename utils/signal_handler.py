79c8e8752d19c8f1e0bf068fd19b80912a9a8cb7
tbahman <66690650+tbahman@users.noreply.github.com>
2025-01-06 23:53:12 -0600

    Update signal_handler.py:register_signal_handler

--- utils/signal_handler.py
+++ utils/signal_handler.py
@@ -5,3 +5,6 @@ def handle_preemption(signum, frame, current_index, results, args_adls_output, c
     save_checkpoint(current_index, results, args_adls_output, checkpoint_ver)
     exit(0)
 
+def register_signal_handler(args_adls_output):
+    signal.signal(signal.SIGTERM, handle_preemption)
+
