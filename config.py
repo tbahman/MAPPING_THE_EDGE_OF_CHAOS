464a4265624c74df83384b82d325b88921943131
tbahman <66690650+tbahman@users.noreply.github.com>
2025-01-10 12:45:57 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
 
-
-
 import argparse
 
-parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument(
-    "--adls_output", default=None, help="Path of the folder on ADLS for output files"
-)
-ARGS = parser.parse_args()
+def parse_arguments():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        "--adls_output",
+        help="path of the folder on ADLS where the training output files are stored",
+        default=None
+    )
+    return parser.parse_args()
 
 
 
